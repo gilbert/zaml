722a894bc2c2a1b4fd63b7d6637aae5c7c1d4f83
Gilbert <gilbertbgarza@gmail.com>
2018-11-18 15:50:59 -0600

    Export parseSchema and createSchema in main file

--- index.ts
+++ index.ts
@@ -2,6 +2,8 @@ import {Pos, ZamlError} from './src/util'
 import {parseSchema, createSchema} from './src/schema'
 import {lex, parseZaml, ParseOptions} from './src/parser'
 
+export {parseSchema, createSchema} from './src/schema'
+
 export function parse (source: string, definitions: string | object, options: ParseOptions={}) {
   if (options.vars && Object.keys(options.vars).length === 0) {
     delete options.vars
